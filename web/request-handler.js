f1bb4c0b02339f000eb6b410f8ffc8304e276f37
Sonny Sheth <sonny.sheth@gmail.com>
2016-02-04 17:53:08 -0200

    Implement solutions to pass tests to return archived website and to return error message

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,27 +1,60 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
+var url = require('url');
+var addUrlToList = require('addUrlToList');
 // require more modules/folders here!
 
-var HTML;
-
-fs.readFile(__dirname + '/public/index.html', 'utf8',  function (err, data) {
-  HTML = data;
-});
 
 
 
-//html = JSON.stringify(html);
-
-//console.log(html);
 exports.handleRequest = function (req, res) {
+
+  var pathName = url.parse(req.url).pathname.split('/');
+  console.log(pathName);
+
   if(req.method === 'GET'){
-    res.writeHead(200, {"Content-Type": "text/html"});
-    /*console.log('before: ' + html)
-    var HTML = JSON.stringify(html);*/
-    console.log('html: ' + HTML);
-    res.write(HTML);
-    res.end(archive.paths.list);
+
+    if (req.url === '/') {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      console.log('inside if');
+      fs.readFile(archive.paths.index, 'utf8',  function (err, data) {
+        res.end(data);
+      });
+
+    }
+
+    else if (pathName[1].indexOf('www.') !== -1){
+      res.writeHead(200, {"Content-Type": "text/html"});
+      console.log('inside else if');
+      res.end(pathName[1]);
+    }
+
+    else {
+      res.writeHead(404);
+      res.end('Got an error!!!!');
+    }
   }
 
 };
+
+// exports.handleRequest = function (req, res) {
+//   if(req.method === 'GET'){
+
+//     if (req.url === '/') {
+//       res.writeHead(200, {"Content-Type": "text/html"});
+//       fs.createReadStream(archive.paths.index).pipe(res);
+//     }
+
+//     else if (req.url === 'www.google.com'){
+//       res.writeHead(200, {"Content-Type": "text/html"});
+//       fs.createReadStream(archive.paths.archivedSites + '/www.google.com/').pipe(res);
+//     }
+
+//     else {
+//       res.writeHead(404);
+//       res.end();
+//     }
+//   }
+
+// };
