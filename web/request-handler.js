17667128969fad3e9c45333104992091f320e8c3
Sonny Sheth <sonny.sheth@gmail.com>
2016-02-03 23:01:21 -0200

    Implement solution to pass first test, GET returns content of index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,27 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
+var HTML;
+
+fs.readFile(__dirname + '/public/index.html', 'utf8',  function (err, data) {
+  HTML = data;
+});
+
+
+
+//html = JSON.stringify(html);
+
+//console.log(html);
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.method === 'GET'){
+    res.writeHead(200, {"Content-Type": "text/html"});
+    /*console.log('before: ' + html)
+    var HTML = JSON.stringify(html);*/
+    console.log('html: ' + HTML);
+    res.write(HTML);
+    res.end(archive.paths.list);
+  }
+
 };
